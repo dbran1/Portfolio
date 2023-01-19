d5e9b0deab955ad7440239e6bb71b7c712ff5161
dbran1 <83590611+dbran1@users.noreply.github.com>
2023-01-19 09:29:46 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,6 @@ function setup() {
 
 function draw() {
   background(220,200,150);
-  fill(Mouse.x%255);
-  ellipse(Mouse.x,Mouse.y, 100,100);
+  fill(mouseX%255, mouseY, mouseX*mouseY);
+  ellipse(mouseX,mouseY, 100,100);
 }
