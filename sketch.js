22412f2c8663cdef233b558139c0cded1f39a83f
dbran1 <83590611+dbran1@users.noreply.github.com>
2023-01-20 11:28:41 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ function setup() {
 
 function draw() {
   background(220,200,150);
-  fill(mouseX%255, mouseY, mouseX*mouseY);
+  fill(mouseX%255, mouseY%255, mouseX*mouseY);
   ellipse(mouseX,mouseY, 10,10);
+  text("Mega Super Cool Portfolio",125,200)
+  text("By: Daegan",160,230)
 }
